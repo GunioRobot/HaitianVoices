ccc4cab5c45344baf6818c5aa329146a1189ebd8
Adam Bair <adambair@gmail.com>
2010-01-23 12:27:14 -0500

    Convert story_test to story_spec
    - Add story factory

--- test/unit/story_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class StoryTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "should be able to get a random story" do
-    assert Story.all.include?(Story.random)
-  end
-end
