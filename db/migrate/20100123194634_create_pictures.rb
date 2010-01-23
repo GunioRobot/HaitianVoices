5fa674f53651008472534b5da4a5001048a32778
Robert Phelps <bobbert0476@gmail.com>
2010-01-23 15:23:43 -0500

    added migrations

---
+++ db/migrate/20100123194634_create_pictures.rb
@@ -0,0 +1,16 @@
+class CreatePictures < ActiveRecord::Migration
+  def self.up
+    create_table :pictures do |t|
+      t.string :caption
+      t.string :photo_file_name # Original filename
+      t.string :photo_content_type # Mime type
+      t.integer :photo_file_size # File size in bytes
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :pictures
+  end
+
+end
