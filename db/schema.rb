e95cbc3f1443abfd2c68b1a9b69190efc8a0d141
Michael Bleigh <michael@intridea.com>
2010-01-23 16:36:09 -0500

    Adding avatar for Twitter/Facebook.

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,15 @@ ActiveRecord::Schema.define(:version => 20100123203459) do
     t.datetime "updated_at"
   end
 
+  create_table "pictures", :force => true do |t|
+    t.string   "caption"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "stories", :force => true do |t|
     t.string   "title"
     t.text     "body"
