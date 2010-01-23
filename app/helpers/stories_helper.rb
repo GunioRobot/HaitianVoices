8e6d871a2f56a8b8f2c76d4999c92984ac978d56
Adam Bair <adambair@gmail.com>
2010-01-23 16:35:31 -0500

    Youtube video urls now embed

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -11,4 +11,13 @@ module StoriesHelper
   def story_filter
     select_tag "filter", options_for_select(["All", "Text", "Audio", "Video"])
   end
+
+  def youtube_embed(url, width=425, height=350)
+    url.gsub!('watch?v=', 'v/')
+    markup =  "<object width='#{width}' height='#{height}'>"
+    markup << "<param name='movie' value='#{url}' align='left'></param>"
+    markup << "<embed src='#{url}' type='application/x-shockwave-flash' width='#{width}' height='#{height}'></embed>"
+    markup << "</object>"
+  end
+  
 end
