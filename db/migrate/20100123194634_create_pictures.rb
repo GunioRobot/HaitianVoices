ed45d05f3e64ba702d18456a31551a699e5e5537
jeremy <jeremy@ivory.(none)>
2010-01-30 10:16:47 -0500

    Bump the photo caption limit to 1000.

--- db/migrate/20100123194634_create_pictures.rb
+++ db/migrate/20100123194634_create_pictures.rb
@@ -1,7 +1,7 @@
 class CreatePictures < ActiveRecord::Migration
   def self.up
     create_table :pictures do |t|
-      t.string :caption
+      t.string :caption, :limit => 1000
       t.string :photo_file_name # Original filename
       t.string :photo_content_type # Mime type
       t.integer :photo_file_size # File size in bytes
