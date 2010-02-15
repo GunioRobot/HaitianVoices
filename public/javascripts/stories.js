a271d8bfdd760e06aacfa99207d279c70947103d
Robert Phelps <bobbert0476@gmail.com>
2010-02-15 13:27:08 -0500

    previewer loading issues mostly fixed; sizing code added.

--- public/javascripts/stories.js
+++ public/javascripts/stories.js
@@ -1,4 +1,5 @@
 jQuery(function($){
+  // called when user adds a photo to a new story
   $('.photo_field').change(function() {
     if ($(this).hasClass('newest')) {
       var new_el = $(this).parent().clone();
@@ -9,19 +10,19 @@ jQuery(function($){
     return false;
   })
 
+  // called when the user hovers over a photo thumbnail
   $('.img_expand').hover(function(e) {
     var preview_img =  $(this).next('span').find('img');
     this.tmp_title = this.title;
     this.title = "";
-    var c = (this.tmp_title != "") ? "<br/>" + this.tmp_title : "";
     if( preview_img.length > 0 )  {
-      $("body").append("<p id='imagepreviewer' style='max-width:" + preview_img.attr('width') + "px'><img src='"+ preview_img.attr('src') +"' alt='Image preview' />"+ c +"</p>");
+      $("body").append("<p id='imagepreviewer' style='max-width:" + preview_img.attr('width') + "px'> \
+                        <img src='"+ preview_img.attr('src') +"' alt='Image preview' /><br />"+ this.tmp_title +"</p>");
       positionPreviewer(e);
     }
     else {
-      $("body").append("<p id='imagepreviewer></p>");
-      positionPreviewer(e);
-      loadImage($(this).attr('pic_url'));
+      $("body").append("<p id='imagepreviewer' style='display:none;'></p>");
+      loadImage(e, $(this).attr('pic_url') + '?size=' + getSize(window.innerHeight, window.innerWidth));
     }
   },
   function() {
@@ -29,8 +30,8 @@ jQuery(function($){
     $("#imagepreviewer").remove();
   });
 
+  // repositions previewer
   $('.img_expand').mousemove(function(e) {
-    // RWP: state logic for picture loading goes here
     positionPreviewer(e);
   });
 
@@ -48,35 +49,50 @@ jQuery(function($){
     return (yDistanceFromBottom > 0) ? 30 : (30 + yDistanceFromBottom);
   }
 
+  // creating body of previewer
   createPreviewer = function(parent_el_id, cap) {
-    $("#imagepreviewer").append($('#' + parent_el_id).find('img').clone()).append(cap);
+    img = $('#' + parent_el_id).find('img');
+    $("#imagepreviewer").append(img.clone()).append('<br />' + cap).css("max-width",img.attr('width') + "px");
   }
 
+  // positioning previewer based on X and Y offset from cursor
   positionPreviewer = function(e) {
     $("#imagepreviewer").css("top",(e.pageY + yOffset(e)) + "px").css("left",(e.pageX + xOffset(e)) + "px");
   }
 
-  loadImage = function(pic_url) {
+  // get image size based on dimensions
+  getSize = function(h, w) {
+    if ((h > 480) && (w > 1440)) { return "L"; }
+    else if ((h > 360) && (w > 960)) { return "M"; }
+    else { return "S"; }
+  }
+
+  // getting parameters for loading image via XML
+  loadImage = function(e, pic_url) {
     $.ajax({
       type: "GET",
       url: pic_url,
       dataType: "xml",
-      success: parsePictureXml
+      success: function(xml) { return parsePictureXml(e, xml) }
     });
   }
 
-  parsePictureXml = function(xml) {
+  // saving image into DOM then loading previewer
+  parsePictureXml = function(e, xml) {
     // find picture and append contents to image viewer
     $(xml).find("picture").each(function()
     {
       var parent_el_id = $(this).find("id").text();
       var photo_url = $(this).find("photo").text();
+      var photo_cap = $(this).find("caption").text();
       var img = new Image();
   
       // run previewer-loading code after image loads
       $(img).load(function () {
-	$('#' + parent_el_id).append($(this));
-	createPreviewer( parent_el_id, $(this).find("caption").text() );
+        $('#' + parent_el_id).append($(this));
+        createPreviewer( parent_el_id, photo_cap );
+        positionPreviewer( e );
+	$("#imagepreviewer").show()
       }).attr('src', photo_url);
       
     });
